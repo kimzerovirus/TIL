56e10be4ce763883bb30f4ee5f555a06e3aa436b
kimzerovirus <yg9699k@naver.com>
2022-04-07 23:55:17 +0900

    시간 복잡도

--- Algorithm/js/test.js
+++ Algorithm/js/test.js
@@ -1,1 +1,31 @@
+const start = new Date();
+
+// setTimeout(() => {
+//     const end = new Date();
+//     console.log(start,end, Math.abs(start-end))
+// }, 3000);
+const array = [];
+for(let i =0; i<10000; i++){
+    array.push(Math.floor(Math.random() * 100));
+    i++;
+}
+
+const newArray = [];
+
+new Promise((resolve)=>{
+    for(let a = 0; a<array.length; a++){
+        let min_index = a;
+        for(let b = a+1; b<array.length; b++){
+            if(array[min_index]>array[b]){
+                min_index = b
+            }
+        }
+        console.log(array[a])
+        array[a] = array[min_index]
+        array[min_index] = array[a]
+        // console.log(array[a], array[min_index], a)
+    }
+    const end = new Date();
+    resolve(console.log(Math.abs(start-end), start, end))
+})
 
