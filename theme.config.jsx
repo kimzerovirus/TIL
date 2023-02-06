12417bcc84fb03861d9c96bd54d07b5faaa7bee3
kimzerovirus <yg9699k@naver.com>
2023-02-07 00:30:11 +0900

    docs 생성

---
+++ theme.config.jsx
@@ -0,0 +1,8 @@
+export default {
+    logo: <span>KIMZEROVIRUS</span>,
+    project: {
+      link: 'https://github.com/kimzerovirus/til',
+    },
+}
+
+// https://nextra.site/docs/docs-theme/theme-configuration
